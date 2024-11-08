1560e27b304ca85bb72731f420315a7b7b1d12fd
Muzammil Murad <mohd.m@algoconsulting.net>
2024-11-08 15:33:44 +0530

    commit

--- scriptForCards.js
+++ scriptForCards.js
@@ -1,16 +1,12 @@
-// Function to toggle the visibility of size-color-section-container based on selected radio button
 function toggleSections(selectedUnit) {
-  // Get all size-color-section-container elements
   const sizeColorSections = document.querySelectorAll(
     ".size-color-section-container"
   );
 
-  // Hide all size-color-section-containers initially
   sizeColorSections.forEach((section) => {
     section.style.display = "none";
   });
 
-  // Display the size-color-section-container for the selected card
   const selectedSection = document.querySelector(
     `.${selectedUnit} .size-color-section-container`
   );
@@ -24,7 +20,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const sizeColorSections = document.querySelectorAll(
     ".size-color-section-container"
   );
-  console.log("????????");
+
   sizeColorSections.forEach((section) => {
     section.style.display = "none";
   });
